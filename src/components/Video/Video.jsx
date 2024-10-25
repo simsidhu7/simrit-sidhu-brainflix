3e427732c560b4f1bc5a170b77e40790c36921f9
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-10-24 21:03:37 -0400

    Update video jsx file to display main image

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,20 +1,18 @@
-import './Video.scss'
-import videoData from '../../data/video-details.json'
+import "./Video.scss";
+import videoData from "../../data/video-details.json";
 
-function MainVideo(){
-const mainVideo = videoData[0];
-return(
-    <div className ="video">
-<div className="video__container">
-<img className="video__image"
-src = {mainVideo.image}
-alt={mainVideo.title}
-/>
-
-
-</div>
-
+function MainVideo() {
+  const mainVideo = videoData[0];
+  return (
+    <div className="video">
+      <div className="video__container">
+        <img
+          className="video__image"
+          src={mainVideo.image}
+          alt={mainVideo.title}
+        />
+      </div>
     </div>
-);
+  );
 }
 export default MainVideo;
--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -1,18 +1,1 @@
 import "./VideoDetails.scss";
-import videoDetails from "..data/video-details.json";
-
-function VideoDetails() {
-  const mainVideo = videoDetails[0];
-  return (
-    <div className="video-details">
-      <div className="video-details__container">
-        <img
-          className="video-details__image"
-          src={mainVideo.image}
-          alt={mainVideo.title}
-        />
-      </div>
-    </div>
-  );
-}
-export default VideoDetails;
