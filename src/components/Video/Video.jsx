d5fb50bd11c1b7e7df58337b3a34ca78a1dc54db
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-10-24 23:56:01 -0400

    Add video controls

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -9,8 +9,33 @@ function MainVideo() {
         <img
           className="video__image"
           src={mainVideo.image}
-          alt={mainVideo.title}
+          alt="Preview of Main Video"
         />
+        <div className="video__controls">
+          <img
+            className="video__play"
+            src="../src/assets/images/play.svg"
+            alt="Video Play Button"
+          />
+            <article className="video__icons">
+                  <img
+            className="video__scrub"
+            src="../src/assets/images/scrub.svg"
+            alt="Video Scrub Button"
+          />
+        
+                  <img
+            className="video__fullscreen"
+            src="../src/assets/images/fullscreen.svg"
+            alt="Video Play Button"
+          />
+                  <img
+            className="video__volume-up"
+            src="../src/assets/images/volume_up.svg"
+            alt="Video Play Button"
+          />
+          </article>
+        </div>
       </div>
     </div>
   );
--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -1,1 +1,18 @@
 import "./VideoDetails.scss";
+import videoData from "../../data/video-details.json";
+
+function VideoDetails() {
+    const videoTitle = videoData[0];
+    return (
+      <div className="video-details">
+        <div className="video-details__container">
+          <img
+            className="video__image"
+            // src={}
+            // alt={}
+          />
+        </div>
+      </div>
+    );
+  }
+  export default MainVideo;
