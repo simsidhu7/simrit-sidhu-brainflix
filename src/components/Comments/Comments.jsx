fe971cac810e646469bfba4591f25384ba6ffef4
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-10-27 01:35:19 -0400

    Format the comments section

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,23 +1,23 @@
 import "./Comments.scss";
 
 function Comments({ comments }) {
-    return(
-  <section className="comments">
-    <h3>Comments</h3>
-    <ul className="comments__list">
-      {comments.map((comment) => (
-        <li key={comment.id} className="comments__list-item">
-          <div className="comments__container">
-            <span className="comments__name">{comments.name}</span>
-            <span className="comments__date">
-              {new Date(comment.timestamp).toLocaleDateString()}
-            </span>
-          </div>
-          <p className="comments__text">{comment.comment}</p>
-        </li>
-      ))}
-    </ul>
-  </section>
+  return (
+    <div className="comments">
+      <ul className="comments__list">
+        {comments.map((comment) => (
+          <li key={comment.id} className="comments__list-item">
+            <div className="comments__container">
+            <p className="comments__image-placeholder"></p>
+              <span className="comments__name">{comment.name}</span>
+              <span className="comments__date">
+                {new Date(comment.timestamp).toLocaleDateString()}
+              </span>
+            </div>
+            <p className="comments__text">{comment.comment}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 export default Comments;
