82e7d5a86a1fe84ff34baa75d223958e0bc6235f
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-10-27 15:38:11 -0400

    Commit tablet display formatting for the comments form

--- src/components/CommentsForm/CommentsForm.jsx
+++ src/components/CommentsForm/CommentsForm.jsx
@@ -5,19 +5,21 @@ function CommentsForm() {
     <section className="comments-form">
       <h3 className="comments-form__comments-amount">3 Comments</h3>
       <form className="comments-form__container">
-        <label htmlFor="comments-form__comment">JOIN THE CONVERSATION</label>
-        <div className="comments-form__input">
-          <img
-            className="comments-form__profile-picture"
-            src="../src/assets/images/Mohan-muruge.jpg"
-            alt="Mohan's picture"
-          />
-          <textarea
-            id="comments-form__comment"
-            name="comment"
-            placeholder="Add a new comment"
-            className="comments-form__commentarea"
-          />
+        <div className="comments-form__input-wrapper">
+          <label htmlFor="comments-form__comment">JOIN THE CONVERSATION</label>
+          <div className="comments-form__input">
+            <img
+              className="comments-form__profile-picture"
+              src="../src/assets/images/Mohan-muruge.jpg"
+              alt="Mohan's picture"
+            />
+            <textarea
+              id="comments-form__comment"
+              name="comment"
+              placeholder="Add a new comment"
+              className="comments-form__commentarea"
+            />
+          </div>
         </div>
         <button type="submit" className="comments-form__button">
           COMMENT
