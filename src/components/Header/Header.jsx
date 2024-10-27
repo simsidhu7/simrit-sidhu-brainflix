5ffb0a4ef9649bfe08c4e718b819f6bad67bef32
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-10-27 16:27:26 -0400

    Push header formatting for the desktop display

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,9 +12,6 @@ function Header() {
           />
         </a>
       </div>
-  
-   
-    
       <section className="header__search-container">
         <input className="header__input" name='search' placeholder="Search" />
         <button className="header__tablet-button">UPLOAD</button>
@@ -27,7 +24,6 @@ function Header() {
           <div className="header__button-container">
           <button className="header__button">UPLOAD</button>
           </div>
-  
     </header>
   );
 }
