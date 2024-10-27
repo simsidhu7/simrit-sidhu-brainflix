85250ddda646e4f4929145d19e95060ff3b28f13
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-10-27 12:22:58 -0400

    Format the Videos List section

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -5,8 +5,7 @@ function VideoList({ videos, selectVideo }) {
 
   return (
     <section className="selection">
-      <h1>Next Videos</h1>
-
+      <h1 className="selection__header">NEXT VIDEOS</h1>
       <ul className="selection__list">
         {videos.map((video) => (
           <li
@@ -14,10 +13,13 @@ function VideoList({ videos, selectVideo }) {
             key={video.id}
             onClick={() => selectVideo(video.id)}
           >
-            <h1 className="selection__title">{video.title}</h1>
-            <p className="selection__channel">{video.channel}</p>
-
-            <img className="selection__video-chosen" src={video.image} />
+            <article className="selection__video-container">
+              <img className="selection__video-chosen" src={video.image} />
+            </article>
+            <article className="selection__video-desr-container">
+              <h3 className="selection__title">{video.title}</h3>
+              <p className="selection__channel">{video.channel}</p>
+            </article>
           </li>
         ))}
       </ul>
