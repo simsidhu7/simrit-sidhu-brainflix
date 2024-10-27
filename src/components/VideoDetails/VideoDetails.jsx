0609376d9a98f682ca406ede7a49d40aff184d7a
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-10-27 14:44:33 -0400

    Format the Video Details containers for mobile display

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -5,34 +5,36 @@ function VideoDetails({ selectedVideo }) {
   return (
     <main className="video">
       <h1 className="video__title">{selectedVideo.title}</h1>
+
       <section className="video__all-details">
         <div className="video__info">
-             <div className="video__data">
-             <p className="video__person">By: {selectedVideo.channel}</p>
-          <img
-            className="video__views"
-            src="../../src/assets/images/views.svg"
-            alt="Views Icon"
-          />
-          {selectedVideo.views} 
-   
-         </div>
-        
-          <div className="video__meta">
+          {/* <div className="video__data"> */}
+          <p className="video__person">By: {selectedVideo.channel}</p>
           <p className="video__date">
             {new Date(selectedVideo.timestamp).toLocaleDateString()}
           </p>
-          <img
-            className="video__likes"
-            src="../../src/assets/images/likes.svg"
-            alt="Likes Icon"
-          />
-          {selectedVideo.likes}
+        </div>
+
+        <div className="video__meta">
+          <div className="video__views-container">
+            <img
+              className="video__views-icon"
+              src="../../src/assets/images/views.svg"
+              alt="Views Icon"
+            />
+            <span className="video__views">{selectedVideo.views} </span>
+          </div>
+          <div className="video__likes-container">
+            <img
+              className="video__likes-icon"
+              src="../../src/assets/images/likes.svg"
+              alt="Likes Icon"
+            />
+            <span className="video__likes">{selectedVideo.likes}</span>
           </div>
         </div>
       </section>
       <p className="video__description">{selectedVideo.description}</p>
-    
     </main>
   );
 }
