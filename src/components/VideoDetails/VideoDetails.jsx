020acbde11fe13c07969332ee201c39a751c7e15
Simrit Sidhu <simmysidhu@ba:0c:14:c3:7c:09.home>
2024-11-01 22:21:43 -0400

    Add functionality to the upload page button and redirect to the homepage

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -4,26 +4,11 @@ import {useParams} from 'react-router-dom'
 import axios from "axios";
 
 function VideoDetails({ selectedVideo }) {
-  // const {id} =useParams();
-  // const apiKey= "84e96018-4022-434e-80bf-000ce4cd12b8";
-  // const url = `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${id}?api_key=${apiKey}`
 
-
-  // const getVideoById = async(id) =>{
-  //   try{
-  //     const response = await axios.get(`${url}`);
-  //   }
-  //   catch(error){
-  //     console.error("Error fetching video by ID", error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getVideoById()}, [id])
 
   return (
     <main className="video">
-      {/* <h1 className="video__title">{selectedVideo.title}</h1> */}
+      <h1 className="video__title">{selectedVideo.title}</h1>
 
       <section className="video__all-details">
         <div className="video__info">
--- src/pages/VideoUploadPage/VideoUploadPage.jsx
+++ src/pages/VideoUploadPage/VideoUploadPage.jsx
@@ -1,27 +1,38 @@
-import "../../App.scss"
+import "../../App.scss";
 import Header from "../../components/Header/Header";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./VideoUploadPage.scss";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function VideoUploadPage() {
- 
-
-return(
-<div className="upload">
-<h2 className="upload__title">Upload Video</h2>
-<form className="upload__form">
-<label for="upload__form-title">TITLE YOUR VIDEO</label>
-    <input id= "upload__form-titlefield" type="text" placeholder ="Add a title to your video" required/>
-    <label for="upload__form-descr">ADD A VIDEO DESCRIPTION</label>
-    <textarea id= "upload__form-descr" placeholder="Add a description to your video" required></textarea>
-    <button type="submit">Upload</button>
-</form>
-
- </div>
-
-);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("The video has been successfully uploaded!");
+    navigate("/");
+  };
+  return (
+    <div className="upload">
+      <h2 className="upload__title">Upload Video</h2>
+      <form className="upload__form" onSubmit={handleSubmit}>
+        <label htmlFor="upload__form-title">TITLE YOUR VIDEO</label>
+        <input
+          id="upload__form-titlefield"
+          type="text"
+          placeholder="Add a title to your video"
+          required
+        />
+        <label htmlFor="upload__form-descr">ADD A VIDEO DESCRIPTION</label>
+        <textarea
+          id="upload__form-descr"
+          placeholder="Add a description to your video"
+          required
+        ></textarea>
+        <button type="submit">Upload</button>
+      </form>
+    </div>
+  );
 }
 
 export default VideoUploadPage;
