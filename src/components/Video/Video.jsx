df3bca6c756e2fe4dd40c5210e64b102464ab802
Simrit Sidhu <simmysidhu@ba:0c:14:c3:7c:09.home>
2024-11-02 14:52:29 -0400

    Format the main video

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,10 +1,12 @@
 import "./Video.scss";
 function Video({ Video }) {
-  return (  
+  return (
     <div className="video__container">
-     { Video ?
-      <video controls poster={Video.image}></video>
-      : <div>Loading</div> }
+      {Video ? (
+        <video className="video__image" controls poster={Video.image}></video>
+      ) : (
+        <div>Loading</div>
+      )}
     </div>
   );
 }
